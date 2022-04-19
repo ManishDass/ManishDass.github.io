ece816110a577c1b1568fc6be1724e3fcafb3a37
Manish <78192346+ManishDass@users.noreply.github.com>
2022-04-19 22:03:42 +0530

    Add files via upload

--- main3.js
+++ main3.js
@@ -1,22 +1,31 @@
-let min = 1;
-let max = 4;
+let min1 = 2;
+let max1 = 5;
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+function getRandomIntInclusive1(min1, max1) {
+    min1 = Math.ceil(min1);
+    max1 = Math.floor(max1);
+    return Math.floor(Math.random() * (max1 - min1 + 1) + min1); //The maximum is inclusive and the minimum is inclusive
+}
+
+let min2 = 2;
+let max2 = 10;
+
+function getRandomIntInclusive2(min2, max2) {
+    min2 = Math.ceil(min2);
+    max2 = Math.floor(max2);
+    return Math.floor(Math.random() * (max2 - min2 + 1) + min2); //The maximum is inclusive and the minimum is inclusive
 }
 
 
-var x = getRandomIntInclusive(min, max)
-var y = getRandomIntInclusive(min, max)
+var x = getRandomIntInclusive1(min1, max1)
+var y = getRandomIntInclusive2(min2, max2)
 
 
 var maxElem = Math.max(x, y)
 var minElem = Math.min(x, y)
 
 
-let arr1 = Array(maxElem * minElem).fill('⚽')
+let arr1 = Array(minElem * maxElem).fill('⚽')
 let finalString1 = arr1.join("");
 
 // let arr2 = Array(minElem).fill('🏐')
@@ -28,7 +37,7 @@ let finalString1 = arr1.join("");
 
 for (let i = 1; i < 10; i++) {
     setTimeout(function myGreeting() {
-        document.getElementById("nums").innerHTML = `${maxElem} x ${minElem} = `;
+        document.getElementById("nums").innerHTML = `${minElem} x ${maxElem} = `;
         document.getElementById("shapes").innerHTML = `${finalString1}  = `;
     }, i * 50);
 }
